c80d10e23e4f72ace2463d7f4f6f43c1fada9397
Roberto Capelo <robertocapelo@Robertos-MBP.home>
2016-10-26 01:45:41 +0100

    removed loop while 1

--- app/models/note.rb
+++ app/models/note.rb
@@ -10,13 +10,11 @@ class Note < ApplicationRecord
 
 
 	def self.random_note
-		
-		while 1
+		random_name = SecureRandom.hex
+		while (Note.find_by_name(random_name) != nil)
 			random_name = SecureRandom.hex
-			
-			if Note.find_by_name(random_name) == nil
-				return Note.new(:name => random_name)
-			end
 		end
+
+		return Note.new(:name => random_name)
 	end
 end
